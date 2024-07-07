4eca3235dbe6818d7728e12e79a288ca543991c6
FeiShengWu <jet20070731@outlook.com>
2024-07-07 19:40:48 +0800

    Add files via upload

--- UserActivityTracker.FileFormat/Structure.cs
+++ UserActivityTracker.FileFormat/Structure.cs
@@ -1,13 +1,22 @@
-﻿namespace UserActivityTracker.FileFormat
+﻿using System.Text.Json.Serialization;
+
+namespace UserActivityTracker.FileFormat
 {
     public class Structure
     {
+        [JsonPropertyName("f")]
         public int FrameRate { get; set; }
 
+        [JsonPropertyName("w")]
         public double StartingWidth { get; set; }
 
+        [JsonPropertyName("h")]
         public double StartingHeight { get; set; }
 
+        [JsonPropertyName("c")]
+        public string StartingConfig { get; set; }
+
+        [JsonPropertyName("a")]
         public string Actions { get; set; }
     }
 }
--- UserActivityTracker/MainWindow.xaml.cs
+++ UserActivityTracker/MainWindow.xaml.cs
@@ -15,7 +15,42 @@ namespace UserActivityTracker.Test
             buttonRecord.IsEnabled = true;
             buttonPlay.IsEnabled = false;
 
-            recorder = new UserActivityTracker.Recorder(this);
+            recorder = new UserActivityTracker.Recorder(this); //Set the element to be recorded to the window.
+
+            //The ContentRendered event can be used to start the recording automatically.
+            //this.ContentRendered += (obj, args) => //Start the recording when the window is ready.
+            //{
+            //    if (this.Tag != null && this.Tag.ToString() == "Play")
+            //    {
+            //        return;
+            //    }
+            //    buttonRecord.IsEnabled = false;
+            //    buttonPlay.IsEnabled = false;
+
+            //    if (!recorder.IsRecording) //Check whether the recording has been started yet.
+            //    {
+            //        recorder.Start(); //Start the recording. Returns true if the recording was started successfully.
+            //        buttonRecord.Content = "Save Session";
+            //        buttonRecord.IsEnabled = true;
+            //    }
+            //};
+
+            //The Closing event can be used to stop the recording automatically.
+            //this.Closing += (obj, args) => //Stop the recording when the window is being closed.
+            //{
+            //    if (this.Tag != null && this.Tag.ToString() == "Play")
+            //    {
+            //        return;
+            //    }
+
+            //    if (recorder.IsRecording) //Check whether the recording has been started yet.
+            //    {
+            //        recorder.Stop(); //Stop the recording. Returns true if the recording was stopped successfully.
+            //        session = recorder.Save(); //Retrieve the string representation of the recording.
+
+            //        MessageBox.Show(session, "Recording Data"); //Display the string representation of the recording.
+            //    }
+            //};
         }
 
         private void RecordButton_Click(object sender, RoutedEventArgs e)
@@ -23,19 +58,19 @@ namespace UserActivityTracker.Test
             buttonRecord.IsEnabled = false;
             buttonPlay.IsEnabled = false;
 
-            if (!recorder.IsRecording)
+            if (!recorder.IsRecording) //Check whether the recording has been started yet.
             {
-                recorder.Start();
+                recorder.Start(); //Start the recording. Returns true if the recording was started successfully.
                 buttonRecord.Content = "Save Session";
                 buttonRecord.IsEnabled = true;
             }
             else
             {
-                recorder.Stop();
+                recorder.Stop(); //Stop the recording. Returns true if the recording was stopped successfully.
                 buttonRecord.Content = "Record Session";
                 buttonRecord.IsEnabled = true;
 
-                session = recorder.Save();
+                session = recorder.Save(); //Retrieve the string representation of the recording.
                 buttonPlay.IsEnabled = true;
             }
         }
@@ -44,17 +79,19 @@ namespace UserActivityTracker.Test
         {
             buttonPlay.IsEnabled = false;
 
-            MainWindow window = new MainWindow();
-            UserActivityTracker.Player player = new UserActivityTracker.Player(window);
+            MainWindow window = new MainWindow() { Tag = "Play" }; //Create a new window for the user actions to be played.
+            UserActivityTracker.Player player = new UserActivityTracker.Player(window); //Set the element to play the user actions to the new window.
 
-            window.Show();
-            window.ContentRendered += async (obj, args) =>
+            window.Show(); //Show the new window.
+            window.ContentRendered += async (obj, args) => //Play the user actions when the new window is ready.
             {
-                if (!player.IsPlaying)
+                if (!player.IsPlaying) //Check whether the playing has been started yet.
                 {
-                    await player.Play(session);
-                    await Task.Delay(500);
-                    window.Close();
+                    await player.Play(session); //Play the recorded user actions from the string representation.
+                    await Task.Delay(500); //Pause for 500 milliseconds before closing the new window.
+                    window.Close(); //Close the new window as the playing is done.
+
+                    MessageBox.Show(session, "Recording Data"); //Display the string representation of the recording.
                 }
             };
 
